7be51b9debbfd6c45a293110462b45e20099e538
camiliamay6 <37019776+camiliamay6@users.noreply.github.com>
2019-12-14 23:56:03 +0700

    Tambah kamera + pergerakan

--- assets/three.module.js
+++ assets/three.module.js
@@ -1,3 +1,4 @@
+
 // Polyfills
 
 if ( Number.EPSILON === undefined ) {
--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-var scene = new THREE.Scene();
+
+import { PointerLockControls } from './assets/PointerLockControls.js';
+
+//inisiasi variabel
+var camera, scene, renderer, controls;
+var maju = false;
+var mundur = false;
+var kiri = false;
+var kanan = false;
+var kecepatanGerak = 0.1;
+
+//Tambah dan set posisi kamera
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 0;
+camera.position.y = 1;
+camera.position.x = 3;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setClearColor ( 0xefefef );
 document.body.appendChild( renderer.domElement );
+
+// var mouseEffect = new ParallaxBarrierEffect(renderer);
+// mouseEffect.setSize(window.innerWidth, window.innerHeight);
+
+
+
 
 // var geometry = new THREE.BoxGeometry( 4, 4, 4 );
 // const loader = new THREE.TextureLoader();
@@ -12,12 +32,11 @@ document.body.appendChild( renderer.domElement );
 // var cube = new THREE.Mesh( geometry, material );
 // scene.add( cube );
 
-camera.position.z = 5;
 
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+var controls = new PointerLockControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.25;
-controls.enableZoom = true;
 
 var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
 keyLight.position.set(-100, 0, 100);
@@ -27,7 +46,9 @@ fillLight.position.set(100, 0, 100);
  
 var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
 backLight.position.set(100, 0, -100).normalize();
- 
+
+//Tambah scene
+var scene = new THREE.Scene();
 scene.add(keyLight);
 scene.add(fillLight);
 scene.add(backLight);
@@ -51,14 +72,79 @@ mtlLoader.load('trail.mtl', function (materials) {
  
 });
 
+var onKeyDown = function(event){
+    switch(event.keyCode){
+        case 87:
+            maju = true;
+            break;
+        case 65:
+            kiri = true;
+            break;
+        case 83:
+            mundur = true;
+            break;
+        case 68:
+            kanan = true;
+            break;        
+    }
+};
 
+var onKeyUp = function(event){
+    switch(event.keyCode){
+        case 87:
+            maju = false;
+            break;
+        case 65:
+            kiri = false;
+            break;
+        case 83:
+            mundur = false;
+            break;
+        case 68:
+            kanan = false;
+            break;        
+    }
+}
+
+document.addEventListener('keydown', onKeyDown, false);
+document.addEventListener('keyup', onKeyUp, false);
+
+// -----------------------------------------FUNCTION-----------------------------//
+
+// function render(){
+//     camera.position.x += (mouseX - camera.position.x) * .05;
+//     camera.position.y += (-mouseY - camera.position.y) * .05;
+//     camera.lookAt(scene.position);
+//     renderer.render(scene, camera);
+    
+    
+// }
+
+function gerakPemain(){
+    if(kanan){
+        camera.position.x -= Math.sin(camera.rotation.y - Math.PI/2) * kecepatanGerak;
+    }
+    if(kiri){
+        camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * kecepatanGerak;
+    }
+    if(maju){
+        camera.position.z -= Math.cos(camera.rotation.y) * kecepatanGerak;
+        camera.position.x -= Math.sin(camera.rotation.y) * kecepatanGerak;
+    }
+    if(mundur){
+        camera.position.z += Math.cos(camera.rotation.y) * kecepatanGerak;
+        camera.position.x += Math.sin(camera.rotation.y) * kecepatanGerak;
+    }
+}
 var animate = function () {
+    renderer.render(scene, camera);
+    gerakPemain();
 	requestAnimationFrame( animate );
-	controls.update();
+	// controls.update();
 	// cube.rotation.x += 0.01;
 	// cube.rotation.y += 0.01;
 
-	renderer.render(scene, camera);
 };
+
 
 animate();
