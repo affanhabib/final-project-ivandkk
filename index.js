5c86339e07f54024eb39629127448c12d4ec9534
Ivan Indrakusuma <ivan.17051@mhs.its.ac.id>
2019-12-16 00:25:28 +0700

    Update model

--- index.js
+++ index.js
@@ -54,18 +54,56 @@ var scene = new THREE.Scene();
 scene.add(keyLight);
 scene.add(fillLight);
 scene.add(backLight);
- 
+
+// // Instantiate a loader
+// var loader = new THREE.GLTFLoader();
+
+// // Optional: Provide a DRACOLoader instance to decode compressed mesh data
+// var dracoLoader = new THREE.DRACOLoader();
+// dracoLoader.setDecoderPath( '' );
+// loader.setDRACOLoader( dracoLoader );
+
+// // Load a glTF resource
+// loader.load(
+// 	// resource URL
+// 	'assets/trail.glb',
+// 	// called when the resource is loaded
+// 	function ( gltf ) {
+
+// 		scene.add( gltf.scene );
+
+// 		gltf.animations; // Array<THREE.AnimationClip>
+// 		gltf.scene; // THREE.Scene
+// 		gltf.scenes; // Array<THREE.Scene>
+// 		gltf.cameras; // Array<THREE.Camera>
+// 		gltf.asset; // Object
+
+// 	},
+// 	// called while loading is progressing
+// 	function ( xhr ) {
+
+// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+// 	},
+// 	// called when loading has errors
+// 	function ( error ) {
+
+// 		console.log( 'An error happened' );
+
+// 	}
+// );
+
 var mtlLoader = new THREE.MTLLoader();
 mtlLoader.setTexturePath('assets/');
 mtlLoader.setPath('assets/');
-mtlLoader.load('trail.mtl', function (materials) {
+mtlLoader.load('fp grafkom.mtl', function (materials) {
  
     materials.preload();
  
     var objLoader = new THREE.OBJLoader();
     objLoader.setMaterials(materials);
     objLoader.setPath('assets/');
-    objLoader.load('trail.obj', function (object) {
+    objLoader.load('fp grafkom.obj', function (object) {
  
         scene.add(object);
         object.position.y -= 0;
